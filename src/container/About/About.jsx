4b27a250b23872fe8443352893368647a9c5903c
MilAd MirzAei <miladmirzaei6802@gmail.com>
2023-05-19 18:51:14 +0430

    Commit 2

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,6 +1,5 @@
 import React,{useState,useEffect} from 'react'
 import {motion} from 'framer-motion'
-import{images} from '../../constants'
 import './About.scss';
 import { AppWrap ,MotionWrap } from '../../wrapper';
 
